9544dee54886af16ac0af6147374fba3946f9dc3
Weslley-Stein <weslley.stein@gmail.com>
2023-12-11 05:53:22 -0300

    feat: working in progress...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Navbar from './Navbar.js';
+import './icon.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{display: 'flex', justifyContent:'center', alignItems:'center', textAling: 'center', flexDirection:'column'}}>
+      <img alt='logo' src='icon.png'/>
+      <p style={{color:'#FFFFFF', fontSize:'32px', display:'block', fontFamily:'IBM Plex Sans'}}>&#128679;Working in progress...&#128679;</p>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+export default function Navbar(){
+  return (
+    <nav className="navbar">
+      <div className="brand">
+        <img alt='test'/>
+        <span>
+          Cloudforge Technologies
+        </span>
+      </div>
+    </nav>
+  )
+};
